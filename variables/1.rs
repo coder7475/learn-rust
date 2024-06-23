d63acb50e76f7d4ea450dbf0ffc11130620bfec6
coder7475 <robiulhossain7475@gmail.com>
2024-06-23 13:11:37 +0600

    feat: learn mutability

--- variables/1.rs
+++ variables/1.rs
@@ -1,13 +1,13 @@
 fn main() {
     // Declare a variable
-    let a_number: i32;
+    let mut a_number: i32 = 10;
         
     // Declare a second variable and bind the value
     let a_word = "Ten";
         
     // Bind a value to the first variable
     // Remove the following comment tag to resolve the compile error
-    a_number = 10;
+    a_number = 15;
     
     println!("The number is {}.", a_number);
     println!("The word is {}.", a_word);
