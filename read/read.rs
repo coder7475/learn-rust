ba07f029dac50b9556cb82be7e4f40eff40edc4a
coder7475 <robiulhossain7475@gmail.com>
2024-06-28 23:09:00 +0600

    feat: read line function improvement

--- read/read.rs
+++ read/read.rs
@@ -1,11 +1,10 @@
-use std::io::{self};
+use std::io;
 
 fn main() {
-  let stdin = io::stdin();
-  
+  // declare a mutable variable
   let mut input = String::new();
-  let _ = stdin.read_line(&mut input);
+  // read a  line
+  let _ = io::stdin().read_line(&mut input);
 
-  println!("You entered: {}", input);
-
+  println!("You entered: {}", input.trim());
 }
