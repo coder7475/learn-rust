68b434fd70b74b8a19f1f23b354eee243bbf7a3c
coder7475 <robiulhossain7475@gmail.com>
2024-07-04 14:05:59 +0600

    feat: initialize car quality project

---
+++ car_quality/target/debug/deps/car_quality-23c0b18452a60b5b.d
@@ -0,0 +1,5 @@
+/home/fahad/rust/learn-rust/car_quality/target/debug/deps/car_quality-23c0b18452a60b5b: src/main.rs
+
+/home/fahad/rust/learn-rust/car_quality/target/debug/deps/car_quality-23c0b18452a60b5b.d: src/main.rs
+
+src/main.rs:
