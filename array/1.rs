3c5905e809f66e98cd0204c51c8a9756652c952c
coder7475 <robiulhossain7475@gmail.com>
2024-07-04 21:57:58 +0600

    learn: how to declare array

---
+++ array/1.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // * declare array, initialize all values, compiler infers length = 7
+    let days = ["Sunday", "Friday", "Saturday"];
+    // * Declare Array, initialize all values to 0, length = 5
+    let bystes = [0; 5];
+}
