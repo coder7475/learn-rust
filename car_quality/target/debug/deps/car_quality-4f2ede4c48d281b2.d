c56941620a78d36fb57a1046a3f13b99fe9ac780
coder7475 <robiulhossain7475@gmail.com>
2024-07-04 16:55:52 +0600

    feat: create car using struct

---
+++ car_quality/target/debug/deps/car_quality-4f2ede4c48d281b2.d
@@ -0,0 +1,5 @@
+/home/fahad/rust/learn-rust/car_quality/target/debug/deps/libcar_quality-4f2ede4c48d281b2.rmeta: src/main.rs
+
+/home/fahad/rust/learn-rust/car_quality/target/debug/deps/car_quality-4f2ede4c48d281b2.d: src/main.rs
+
+src/main.rs:
---
+++ car_quality/target/debug/deps/car_quality-5158f66a9a0e3d0c.d
@@ -0,0 +1,5 @@
+/home/fahad/rust/learn-rust/car_quality/target/debug/deps/libcar_quality-5158f66a9a0e3d0c.rmeta: src/main.rs
+
+/home/fahad/rust/learn-rust/car_quality/target/debug/deps/car_quality-5158f66a9a0e3d0c.d: src/main.rs
+
+src/main.rs:
