0c791131c5280c4d29f7c23990328a71c0dddd28
coder7475 <robiulhossain7475@gmail.com>
2024-07-04 21:20:56 +0600

    feat: finish car quality function

---
+++ car_quality/target/debug/deps/car_quality-ea49869652cbdf95.d
@@ -0,0 +1,5 @@
+/home/fahad/projects/rust/learn-rust/car_quality/target/debug/deps/car_quality-ea49869652cbdf95: src/main.rs
+
+/home/fahad/projects/rust/learn-rust/car_quality/target/debug/deps/car_quality-ea49869652cbdf95.d: src/main.rs
+
+src/main.rs:
---
+++ car_quality/target/debug/deps/car_quality-f88fa884029eaed0.d
@@ -0,0 +1,5 @@
+/home/fahad/projects/rust/learn-rust/car_quality/target/debug/deps/libcar_quality-f88fa884029eaed0.rmeta: src/main.rs
+
+/home/fahad/projects/rust/learn-rust/car_quality/target/debug/deps/car_quality-f88fa884029eaed0.d: src/main.rs
+
+src/main.rs:
