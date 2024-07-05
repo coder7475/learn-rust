aab59ba04c41bc26836e6b1e19d30a0c2dfe80b4
coder7475 <robiulhossain7475@gmail.com>
2024-07-05 11:22:13 +0600

    feat: learn array

--- array/1.rs
+++ array/1.rs
@@ -1,6 +1,10 @@
 fn main() {
     // * declare array, initialize all values, compiler infers length = 7
     let days = ["Sunday", "Friday", "Saturday"];
+    // access the days values
+    let first = days[0];
+    let second = days[1];
     // * Declare Array, initialize all values to 0, length = 5
-    let bystes = [0; 5];
+    let bytes = [0; 5];
+
 }
