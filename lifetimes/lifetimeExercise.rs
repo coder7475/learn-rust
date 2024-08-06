9365b3e7ba5fcae4a5cbc45adc2580dd3f811946
coder7475 <robiulhossain7475@gmail.com>
2024-08-06 19:11:19 +0600

    feat: lifetime exercise

--- lifetimes/lifetimeExercise.rs
+++ lifetimes/lifetimeExercise.rs
@@ -1,6 +1,7 @@
-// TODO: modify only this function.
-fn copy_and_return(vector: &mut Vec<String>, value: &str) {
+
+fn copy_and_return<'a>(vector: &'a mut Vec<String>, value: &'a str) -> &'a String {
     vector.push(String::from(value));
+    vector.get(vector.len() - 1).unwrap()
 }
 
 fn main() {
