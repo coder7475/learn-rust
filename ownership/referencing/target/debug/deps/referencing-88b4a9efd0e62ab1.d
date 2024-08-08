2db8faa1c421574b6f47686155ca4774a45b5d05
coder7475 <robiulhossain7475@gmail.com>
2024-08-08 07:05:23 +0600

    feat: learn referencing &

---
+++ ownership/referencing/target/debug/deps/referencing-88b4a9efd0e62ab1.d
@@ -0,0 +1,5 @@
+/home/fahad/rust/learn-rust/ownership/referencing/target/debug/deps/referencing-88b4a9efd0e62ab1: src/main.rs
+
+/home/fahad/rust/learn-rust/ownership/referencing/target/debug/deps/referencing-88b4a9efd0e62ab1.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/referencing/target/debug/referencing.d
@@ -0,0 +1,1 @@
+/home/fahad/rust/learn-rust/ownership/referencing/target/debug/referencing: /home/fahad/rust/learn-rust/ownership/referencing/src/main.rs
