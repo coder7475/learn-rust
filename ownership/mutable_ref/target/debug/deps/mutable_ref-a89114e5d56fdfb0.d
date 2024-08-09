47c07fd15a9643384acb09c8fe57bd5580d334c7
coder7475 <robiulhossain7475@gmail.com>
2024-08-09 19:04:29 +0600

    feat: learn mutable reference

---
+++ ownership/mutable_ref/target/debug/deps/mutable_ref-a89114e5d56fdfb0.d
@@ -0,0 +1,5 @@
+/home/fahad/rust/learn-rust/ownership/mutable_ref/target/debug/deps/mutable_ref-a89114e5d56fdfb0: src/main.rs
+
+/home/fahad/rust/learn-rust/ownership/mutable_ref/target/debug/deps/mutable_ref-a89114e5d56fdfb0.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/mutable_ref/target/debug/mutable_ref.d
@@ -0,0 +1,1 @@
+/home/fahad/rust/learn-rust/ownership/mutable_ref/target/debug/mutable_ref: /home/fahad/rust/learn-rust/ownership/mutable_ref/src/main.rs
