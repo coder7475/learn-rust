33fbe8d253e5c51bbb39644ad292ceaec7371d7f
coder7475 <robiulhossain7475@gmail.com>
2024-10-30 18:49:59 +0600

    feat: learn string type

--- ownership/string_type.rs
+++ ownership/string_type.rs
@@ -3,7 +3,13 @@
 fn main() {
     {                      // s is not valid here, it’s not yet declared
         let s = "hello";   // s is valid from this point forward
-
+        
         // do stuff with s
     }                      // this scope is now over, and s is no longer valid
+
+    let mut s = String::from("hello");
+
+    s.push_str(", world!");
+
+    println!("{s}");
 }
