d93339c2d9b95c62c55bce331fefd385cf5944ba
coder7475 <robiulhossain7475@gmail.com>
2024-11-01 07:33:19 +0600

    feat: learn dangling pointer 2

--- referencesdangling_pointer.rs
+++ referencesdangling_pointer.rs
@@ -2,9 +2,10 @@ fn main() {
     let reference_to_nothing = dangle();
 }
 
-fn dangle() -> &String {
-    let s = String::from("hello");
+fn dangle() -> &String { // * dangle returns a reference to a String
 
-    &s
-}
-    
+    let s = String::from("hello"); // * s is a new String
+
+    &s // *we return a reference to the String, s
+} // *Here, s goes out of scope, and is dropped. Its memory goes away.
+  // ! Danger!
